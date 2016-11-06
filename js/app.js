9d0bfd6bcab3f445312da695e8e876570554e0ac
Przemysław Borowski <p.borowski@onet.eu>
2016-11-06 02:23:58 +0100

    small changes and improvements

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    // ------ SLIDER
+    //all variables
+    var leftArrow = document.querySelector('.left-arrow');
+    var rightArrow = document.querySelector('.right-arrow');
+    var imgSlider = document.querySelectorAll('.slide');
+    var imgCounter = 0; // counter with actual img index
+    imgSlider[0].classList.add('visible');
+
+    //events for arrows
+    rightArrow.addEventListener('click', function() {
+        //remove class visible with actual img index
+        imgSlider[imgCounter].classList.remove('visible');
+        //increase actual img index
+        imgCounter++;
+        if (imgCounter >= imgSlider.length) {
+            imgCounter = 0;
+        }
+        imgSlider[imgCounter].classList.add('visible');
+    });
+
+    leftArrow.addEventListener('click', function() {
+        imgSlider[imgCounter].classList.remove('visible');
+        imgCounter--;
+        if (imgCounter < 0) {
+            imgCounter = imgSlider.length - 1;
+        }
+        imgSlider[imgCounter].classList.add('visible');
+    });
+
+
     // hide & show text Chair CLAIR & MARGARITA from "bar-text" after mouseover & mouseout event:
     var imageGallery = document.querySelectorAll('.image-gallery');
 
@@ -15,29 +45,5 @@ document.addEventListener('DOMContentLoaded', function() {
             // this.querySelector('.opacity-gallery').style.display='block';
         });
     }
-
-    var rightArrow = document.querySelector('.right-arrow');
-    var leftArrow = document.querySelector('.left-arrow');
-    var slideImg = document.querySelectorAll('.slide');
-    var imgCounter = 0;
-    slideImg[0].classList.add('visible');
-
-    rightArrow.addEventListener("click", function() {
-        slideImg[imgCounter].classList.remove('visible');
-        imgCounter++;
-        if (imgCounter >= slideImg.length) {
-            imgCounter = 0;
-        }
-        slideImg[imgCounter].classList.add('visible');
-    });
-
-    leftArrow.addEventListener("click", function() {
-        slideImg[imgCounter].classList.remove('visible');
-        imgCounter--;
-        if (imgCounter < 0) {
-            imgCounter = slideImg.length - 1;
-        }
-        slideImg[imgCounter].classList.add('visible');
-    });
 
 });
